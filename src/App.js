5e6022fa5b187aeaca4042fb1e12bb9369017295
Yogga Aditya Candra <yogga910@gmail.com>
2022-12-21 14:28:57 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import './App.css';
-import { Container } from 'reactstrap';
-// import UsersComponent from './Components/UsersComponent';
-import { useEffect, useState } from 'react';
-import kab_2014 from './Data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Kab2014 from './Pages/Kab2014';
 
 function App() {
-  const [tempData, setTempData] = useState([]);
-
-  const filterData = () => {
-    const kabupaten = kab_2014.filter((item) => item.Provinsi === 'Aceh');
-
-    const deleteDot = kabupaten.map((item) => {
-      return {
-        ...item,
-        Luas_Areal_2014: item.Luas_Areal_2014.replace('.', ''),
-      };
-    });
-
-    const sortDescbyLuasAreal = deleteDot.sort(
-      (a, b) => parseInt(b.Luas_Areal_2014) - parseInt(a.Luas_Areal_2014)
-    );
-
-    // logging
-    console.log('sortDescbyLuasAreal', sortDescbyLuasAreal);
-
-    setTempData(sortDescbyLuasAreal);
-  };
-
-  useEffect(() => {
-    filterData();
-  }, []);
-
   return (
-    <div className='App'>
-      <Container fluid>
-        {/* <UsersComponent /> */}
-
-        {tempData.map((item) => {
-          return (
-            <div key={item.No} className='dflex gap-3'>
-              <h1>{item.Kabupaten_Kota}</h1>
-              <h1>{item.Luas_Areal_2014}</h1>
-            </div>
-          );
-        })}
-      </Container>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/kab-2014' element={<Kab2014 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Kab2014.js
+++ src/Pages/Kab2014.js
@@ -1,9 +1,125 @@
+// import { Grid } from 'gridjs-react';
 import React, { Fragment } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Table } from 'reactstrap';
+import kab_2014 from '../Data.js';
+
+// Module not found: Error: Can't resolve 'gridjs-react' in 'C:\Users\user\Documents\GitHub\react-gridjs\src\Pages' how can i fix this error ? i already install gridjs-react but still error like this ?
 
 const Kab2014 = () => {
+  // const [jumlahBaris, setJumlahBaris] = useState(5);
+  const [tempData, setTempData] = useState([]);
+
+  const filterData = () => {
+    // const kabupaten = kab_2014.filter((item) => item.Provinsi === 'Aceh');
+
+    const deleteDot = kab_2014.map((item) => {
+      return {
+        ...item,
+        Luas_Areal_2014: item.Luas_Areal_2014.replace('.', ''),
+        Produksi_2014: item.Produksi_2014.replace('.', ''),
+        TBM: item.TBM.replace('.', ''),
+        TM: item.TM.replace('.', ''),
+        TR: item.TR.replace('.', ''),
+      };
+    });
+
+    setTempData(deleteDot);
+  };
+
+  useEffect(() => {
+    filterData();
+  }, []);
+
   return (
     <Fragment>
       <p>Kab2014</p>
+
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Kabupaten / Kota</th>
+            <th>Luas Areal 2014</th>
+            <th>Produkti</th>
+            <th>Produksi</th>
+            <th>Jumlah Petani</th>
+            <th>TBM</th>
+            <th>TM</th>
+            <th>TR</th>
+            <th>Provinsi</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tempData.map((item, i) => (
+            <tr key={i}>
+              <th scope='row'>{i + 1}</th>
+              <td>{item.Kabupaten_Kota}</td>
+              <td>{item.Luas_Areal_2014}</td>
+              <td>{item.Produktiv_2014}</td>
+              <td>{item.Produksi_2014}</td>
+              <td>{item.Jumlah_Petani}</td>
+              <td>{item.TBM}</td>
+              <td>{item.TM}</td>
+              <td>{item.TR}</td>
+              <td>{item.Provinsi}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      {/* <select
+        className='form-select form-select-md'
+        value={jumlahBaris}
+        onChange={(e) => setJumlahBaris(e.target.value)}
+      >
+        <option value={5}>Jumlah Baris</option>
+        <option value={10}>10</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
+        <option value={9999999}>semua</option>
+      </select> */}
+
+      {/* <Grid
+        data={() =>
+          deleteDot.map((item, i) => ({
+            ...item,
+            number: i + 1,
+          }))
+        }
+        columns={[
+          {
+            id: 'number',
+            name: 'No',
+          },
+          {
+            data: (row) => row,
+            name: 'Kabupaten / Kota',
+            width: '30%',
+            formatter: (cell) =>
+              _(
+                <>
+                  <p>{cell.Kabupaten_Kota}</p>
+                </>
+              ),
+          },
+        ]}
+        search={{
+          selector: (cell, rowIndex, cellIndex) =>
+            cellIndex === 1 ? cell.Provinsi : cell,
+        }}
+        language={{
+          search: {
+            placeholder: 'Search By Title...',
+          },
+        }}
+        pagination={{
+          enabled: true,
+          limit: jumlahBaris || 10,
+          resetPageOnUpdate: true,
+        }}
+      /> */}
     </Fragment>
   );
 };

6c556b8fbdfc0a2c4868c4eaed989b54e26e6313
Yogga Aditya Candra <yogga910@gmail.com>
2022-12-26 14:48:19 +0700

    update 2017 map

--- src/Pages/Cocoa/2017/index.js
+++ src/Pages/Cocoa/2017/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Kab2017 from './Kab2017';
 import Prov2017 from './Prov2017';
 import Story17 from './Story17';
+import map_2017 from '../../../assets/img/2017_map.png';
 
 const index = () => {
   return (
     <div>
-      <h1>Cococa 2017</h1>
-      <h1>Map on Top</h1>
+      <h1 className='text-center'>
+        Keadaan Komoditas Kakao di Indonesia pada tahun 2017
+      </h1>
+      <div className='d-flex justify-content-center align-baseline py-0 my-0'>
+        <img src={map_2017} alt='map_2017' />
+      </div>
+
+      <div className='d-flex justify-content-center align-baseline mb-5'>
+        <a
+          href='https://2017-map.netlify.app'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          Lihat Peta Sebaran
+        </a>
+      </div>
 
       <Prov2017 />
       <Kab2017 />

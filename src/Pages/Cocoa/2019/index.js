3c93abf552049cde4f4cf14d201893e6ad44e9e0
Yogga Aditya Candra <yogga910@gmail.com>
2022-12-31 16:34:10 +0700

    2019 map

--- src/Pages/Cocoa/2019/index.js
+++ src/Pages/Cocoa/2019/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Kab2019 from './Kab2019';
 import Prov2019 from './Prov2019';
 import Story19 from './Story19';
+import map_2019 from '../../../assets/img/2019_map.png';
 
 const index = () => {
   return (
     <div>
-      <h1>Cococa 2019</h1>
-      <h1>Map on Top</h1>
+      <h1 className='text-center'>
+        Keadaan Komoditas Kakao di Indonesia pada tahun 2019
+      </h1>
+      <div className='d-flex justify-content-center align-baseline py-0 my-0'>
+        <img src={map_2019} alt='Peta 2019' />
+      </div>
+
+      <div className='d-flex justify-content-center align-baseline mb-5'>
+        <a
+          href='https://2019-map.netlify.app/'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          Lihat Peta Sebaran
+        </a>
+      </div>
 
       <Prov2019 />
       <Kab2019 />

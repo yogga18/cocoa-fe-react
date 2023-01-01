d6a72c0ed3e911fc233233022d189d04bc7c29c0
Yogga Aditya Candra <yogga910@gmail.com>
2023-01-02 00:49:29 +0700

    adding 2016 map deploy on vercel

--- src/Pages/Cocoa/2016/Kab2016.js
+++ src/Pages/Cocoa/2016/Kab2016.js
@@ -1,7 +1,211 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import kab_2016 from '../../../Data/Kabupaten/Data16';
+import { Grid, _ } from 'gridjs-react';
 
 const Kab2016 = () => {
-  return <div>Belum ada datanya</div>;
+  const [jumlahBaris, setJumlahBaris] = useState(10);
+  const [tempData, setTempData] = useState([]);
+
+  const filterData = () => {
+    const deleteDot = kab_2016.map((item) => {
+      return {
+        ...item,
+        Luas_Areal_2016: item.Luas_Areal_2016.replace('.', ''),
+        Produksi_2016: item.Produksi_2016.replace('.', ''),
+        TBM: item.TBM.replace('.', ''),
+        TM: item.TM.replace('.', ''),
+        TR: item.TR.replace('.', ''),
+      };
+    });
+
+    setTempData(deleteDot);
+  };
+
+  useEffect(() => {
+    filterData();
+  }, []);
+
+  return (
+    <Fragment>
+      <Container fluid className='my-5'>
+        <Row className='mx-3'>
+          <Col md='6'>
+            Tabel Penghasil Komoditas Kakao 2016 by Kabupaten di Indonesia
+          </Col>
+          <Col md='6'>
+            <select
+              className='form-select form-select-md'
+              value={jumlahBaris}
+              onChange={(e) => setJumlahBaris(e.target.value)}
+            >
+              <option value={10}>Jumlah Baris</option>
+              <option value={25}>25</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+              <option value={9999999}>semua</option>
+            </select>
+          </Col>
+        </Row>
+
+        <Row className='pt-5'>
+          <Col md='1'></Col>
+          <Col md='10'>
+            <Grid
+              data={() =>
+                tempData.map((item, i) => ({
+                  ...item,
+                  number: i + 1,
+                }))
+              }
+              columns={[
+                {
+                  id: 'number',
+                  name: 'No',
+                },
+                {
+                  data: (row) => row,
+                  name: 'Kabupaten / Kota',
+                  width: '15%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        <p>{cell.Kabupaten_Kota}</p>
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'Luas Areal',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        {cell.Luas_Areal_2016 === '' ? (
+                          <p>-</p>
+                        ) : (
+                          <p>{cell.Luas_Areal_2016}</p>
+                        )}
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'Produktivitas',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        {cell.Produktiv_2016 === '' ? (
+                          <p>-</p>
+                        ) : (
+                          <p>{cell.Produktiv_2016}</p>
+                        )}
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'Produksi',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        {cell.Produksi_2016 === '' ? (
+                          <p>-</p>
+                        ) : (
+                          <p>{cell.Produksi_2016}</p>
+                        )}
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'Jumlah Petani',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        {cell.Jumlah_Petani === '' ? (
+                          <p>-</p>
+                        ) : (
+                          <p>{cell.Jumlah_Petani}</p>
+                        )}
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'TBM',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        <>{cell.TBM === '' ? <p>-</p> : <p>{cell.TBM}</p>}</>
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'TM',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        <>{cell.TM === '' ? <p>-</p> : <p>{cell.TM}</p>}</>
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'TR',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        <>{cell.TR === '' ? <p>-</p> : <p>{cell.TR}</p>}</>
+                      </>
+                    ),
+                },
+                {
+                  data: (row) => row,
+                  name: 'Provinsi',
+                  width: '10%',
+                  formatter: (cell) =>
+                    _(
+                      <>
+                        <>
+                          {cell.Provinsi === '' ? (
+                            <p>-</p>
+                          ) : (
+                            <p>{cell.Provinsi}</p>
+                          )}
+                        </>
+                      </>
+                    ),
+                },
+              ]}
+              search={{
+                selector: (cell, rowIndex, cellIndex) =>
+                  cellIndex === 1 ? cell.Provinsi : cell,
+              }}
+              language={{
+                search: {
+                  placeholder: 'Search By Title...',
+                },
+              }}
+              pagination={{
+                enabled: true,
+                limit: jumlahBaris || 10,
+                resetPageOnUpdate: true,
+              }}
+            />
+          </Col>
+          <Col md='1'></Col>
+        </Row>
+      </Container>
+    </Fragment>
+  );
 };
 
 export default Kab2016;
--- src/Pages/Cocoa/2016/index.js
+++ src/Pages/Cocoa/2016/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Kab2016 from './Kab2016';
 import Prov2016 from './Prov2016';
 import Story16 from './Story16';
+import map_2016 from '../../../assets/img/2015_map.png';
 
 const index = () => {
   return (
     <div>
-      <h1>Cococa 2016</h1>
-      <h1>Map on Top</h1>
+      <h1 className='text-center'>
+        Keadaan Komoditas Kakao di Indonesia pada tahun 2016
+      </h1>
+      <div className='d-flex justify-content-center align-baseline py-0 my-0'>
+        <img src={map_2016} alt='Map 2016' />
+      </div>
+
+      <div className='d-flex justify-content-center align-baseline mb-5'>
+        <a
+          href='https://2016-map.vercel.app/'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          Lihat Peta Sebaran
+        </a>
+      </div>
 
       <Prov2016 />
       <Kab2016 />

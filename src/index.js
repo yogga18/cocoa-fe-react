ebd84a630f4161c3d79c9b2050c805ef7eafb6e1
Yogga Aditya Candra <yogga910@gmail.com>
2023-01-11 00:58:25 +0700

    g

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'react-vertical-timeline-component/style.min.css';
-import 'boxicons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
